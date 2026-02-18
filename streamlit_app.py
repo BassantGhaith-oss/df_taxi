7bc9503ee279a2ffffbbc76502c06ffbc8489e1a
BassantGhaith-oss <bassantmohamadamen@gmail.com>
2026-02-18 15:05:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
-st.title('🚕 Pick up Trip')
 
+st.title('🚕 Pick up Trip')
 st.info('welcome to our taxi driver app based on machine learning')
-df=pd.read_csv('')
+
+model = joblib.load("model.pkl")
+
+distance = st.number_input("Distance")
+passengers = st.number_input("Passengers")
+
+if st.button("Predict"):
+    X = np.array([[distance, passengers]])
+    pred = model.predict(X)
+    st.write("Price:", pred[0])
+
